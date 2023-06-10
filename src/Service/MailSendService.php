756eebe08f60f980a1903cea7a3c4ea8e369ccc4
intinalambrico <web@internetinalambrico.com.co>
2023-06-10 15:54:46 -0500

    creación de mailrelay

--- src/Service/MailSendService.php
+++ src/Service/MailSendService.php
@@ -24,9 +24,9 @@ final class MailSendService
         //partimos la información 
         $partes = explode("@" , $html->settings_valor);
 
-        $body = $partes[0]." ".$usuarios->usuario_nombre_completo." </br>".$partes[1];
+        $body = $partes[0]." ".$usuarios->usuario_nombre_completo.$partes[1];
 
-        $cabeceras = 'MIME-Version: 1.0'. "\r\n";
+        /*$cabeceras = 'MIME-Version: 1.0'. "\r\n";
         $cabeceras .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
         $cabeceras .= 'From: Puntos Merkas<gerencia@merkas.co>'. "\r\n";
 
@@ -38,7 +38,60 @@ final class MailSendService
         if(!mail($usuarios->usuario_correo , $subject , $msm , $cabeceras))
         {
             throw new \Exception("Error enviando el correo" , 400);
-        }
+        }*/
+        $curl = curl_init();
+
+        $jayParsedAry = [
+            "from" => [
+                  "email" => "no-reply@merkas.co", 
+                  "name" => "Puntos Merkas" 
+               ], 
+            "to" => [
+                     [
+                        "email" => $usuarios->usuario_correo, 
+                        "name" => $usuarios->usuario_nombre_completo 
+                     ] 
+                  ], 
+            "subject" => "Registro Exitoso", 
+            "html_part" => $body, 
+            "text_part" => "", 
+            "text_part_auto" => false, 
+            "headers" => [
+                           "X-CustomHeader" => "Header value" 
+                        ], 
+            "smtp_tags" => [
+                              "string" 
+                           ] 
+         ]; 
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://merkas.ipzmarketing.com/api/v1/send_emails",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => json_encode($jayParsedAry),
+            CURLOPT_HTTPHEADER => array(
+              "content-type: application/json",
+              "x-auth-token: P_8czBc5b4Leznavv_VfeYCSAsmy8VxnsuUthMnq"
+            ),
+          ));
+          
+          $response = curl_exec($curl);
+          $err = curl_error($curl);
+          
+          curl_close($curl);
+          
+          if ($err) {
+            throw new \Exception('Error enviando correo', 400);
+          } else {
+            
+            return $usuarios;
+          }
+
+
 
 
 
