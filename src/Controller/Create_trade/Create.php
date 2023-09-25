007600c8b5f3fc5e597e7297302d40f109292206
intinalambrico <web@internetinalambrico.com.co>
2023-09-25 10:10:26 -0500

    res

--- src/Controller/Create_trade/Create.php
+++ src/Controller/Create_trade/Create.php
@@ -17,7 +17,7 @@ final class Create extends Base
         $trade = $this->getTradeService()->create($input);
           
         $resp =  new \stdclass();
-        $resp->resp = "successful";
+        $resp->res = "successful";
 
         return $response->withJson($resp, StatusCodeInterface::STATUS_CREATED);
     }
